25bbfea0778aaac0f938aece2716de29f673bbe7
dPenedo <dpenedo@tutanota.com>
2023-11-13 21:19:29 -0300

    controladores y servicios empezados

--- src/main/java/com/example/biblioteca/service/BibliotecaService.java
+++ src/main/java/com/example/biblioteca/service/BibliotecaService.java
@@ -1,4 +1,10 @@
 package com.example.biblioteca.service;
 
+import com.example.biblioteca.entity.Libro;
+
+import java.util.List;
+
 public interface BibliotecaService {
+    public List<Libro> mostrarLibros();
+
 }
--- src/main/java/com/example/biblioteca/service/impl/BibliotecaServiceImpl.java
+++ src/main/java/com/example/biblioteca/service/impl/BibliotecaServiceImpl.java
@@ -1,7 +1,19 @@
 package com.example.biblioteca.service.impl;
 
+import com.example.biblioteca.entity.Libro;
+import com.example.biblioteca.repository.LibroRepository;
+import com.example.biblioteca.service.BibliotecaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class BibliotecaServiceImpl {
+public class BibliotecaServiceImpl implements BibliotecaService {
+    @Autowired
+    private LibroRepository libroRepository;
+    @Override
+    public List<Libro> mostrarLibros() {
+        return (List<Libro>) libroRepository.findAll();
+    }
 }
